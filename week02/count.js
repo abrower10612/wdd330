f7429d82947a9bf511eafc2899c2bb9bd75f8561
abrower10612 <abrower7@byui.edu>
2021-02-04 16:04:45 -0700

    asdf

--- week02/count.js
+++ week02/count.js
@@ -1,12 +1,11 @@
 function addFunction() {
   let num = document.getElementById("number").value;
   let i = parseInt(num);
-  while (Number.isInteger(i) == false) {
+  if (Number.isInteger(i) == false || num <= 0) {
     document.getElementById("output").innerHTML = "Please enter a valid positive number";
-    break
   }
-  if (Number.isInteger(i)) {
-    document.getElementById("output").innerHTML = "Good job";
+  else {
+    document.getElementById("output").innerHTML = "Good job, you correctly entered a posistive integer.";
   }
 
 }
