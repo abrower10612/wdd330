a7198eca48a24ab5ab8f684c3b56f8afb9e7ad61
abrower10612 <abrower7@byui.edu>
2021-03-08 15:32:27 -0700

    asdf

--- week09/final-project-proposal/js/scripts.js
+++ week09/final-project-proposal/js/scripts.js
@@ -1,9 +1,13 @@
 function openNav() {
+  document.querySelector("main").style.opacity = "50%";
+  document.getElementById("headerContent").style.opacity = "50%";
   document.getElementById("mySidenav").style.width = "175px";
   mySidenav.classList.toggle("menuBorder");
 }
 
 function closeNav() {
+  document.getElementById("headerContent").style.opacity = "100%";
+  document.querySelector("main").style.opacity = "100%";
   document.getElementById("mySidenav").style.width = "0";
   mySidenav.classList.toggle("menuBorder");
 }
