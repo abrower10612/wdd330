0d12e2ba936feff671f64b3a9af7690da79abd3e
abrower10612 <abrower7@byui.edu>
2021-03-08 14:50:24 -0700

    asdf

--- week09/final-project-proposal/js/scripts.js
+++ week09/final-project-proposal/js/scripts.js
@@ -1,7 +1,9 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "175px";
+  mySidenav.classList.toggle("menuBorder");
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  mySidenav.classList.toggle("menuBorder");
 }
